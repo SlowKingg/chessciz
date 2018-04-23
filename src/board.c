dbb4197dd813d702de0ff66485a17df18fb1eab1
Kirill <nagavi25599@gmail.com>
2018-04-23 21:30:52 +0700

    Add Rook and Horse check

--- src/board.c
+++ src/board.c
@@ -1,60 +1,37 @@
-
 #include "board.h"
-#include <stdlib.h>
 #include <ctype.h>
 
 int move (char *s, Board *board) {
 
-	char *m1 = calloc (4, sizeof (char));
-		if (m1 == NULL) {
-			return -1;
-		}
+	char m1 [4];
 
-	char *m2 = calloc (4, sizeof (char));
-		if (m1 == NULL) {
-			free (m1);
-			return -1;
-		}
+	char m2 [4];
 
 	if (str_to_moves (s, m1, m2) == 2) {
 		if (move_check (m1, board) == 0) {
 			if (move_make (m1, board)) {
-				free (m1);
-				free (m2);
 				return -1;
 			}
 		} else {
-			free (m1);
-			free (m2);
 			return -1;
 		}
 
 		if (move_check (m2, board) == 0) {
 			if (move_make (m2, board)) {
-				free (m1);
-				free (m2);
 				return -1;
 			}
 		} else {
-			free (m1);
-			free (m2);
 			return -1;
 		}
 	} else if (str_to_moves (s, m1, m2) == 1) {
 		if (move_check (m1, board) == 0) {
 			if (move_make (m1, board)) {
-				free (m1);
-				free (m2);
 				return -1;
 			}
 		} else {
-			free (m1);
-			free (m2);
 			return -1;
 		}
 	} else {
-		free (m1);
-		free (m2);
 		return -1;
 	}
 
@@ -125,8 +102,8 @@ int str_to_moves (char *s, char *move1, char *move2) {
 
 int move_check (char *move, Board *board) {
 
-	int *st = calloc (2, sizeof (int));
-	int *en = calloc (2, sizeof (int));
+	int st[2];
+	int en[2];
 
 	if (get_coord (move, st, en)) {
 		return -1;
@@ -136,8 +113,6 @@ int move_check (char *move, Board *board) {
 	int coldif = st[1] - en[1];
 
 	if (board->cell[st[0]][st[1]].fill == false) {
-		free (st);
-		free (en);
 		return -1;
 	}
 
@@ -146,43 +121,83 @@ int move_check (char *move, Board *board) {
 		if ((linedif == 1 || linedif == -1) && coldif == 0) {
 			if (board->cell[st[0]][st[1]].piececolor == White &&
 				 											linedif == -1) {
-				free (st);
-				free (en);
 				return -1;
 			}
-			if (board->cell[st[0]][st[1]].piececolor == Black &&\
+			if (board->cell[st[0]][st[1]].piececolor == Black &&
 				 											linedif == 1) {
-				free (st);
-				free (en);
 				return -1;
 			}
-			free (st);
-			free (en);
 			return 0;
-		} else if ((linedif == 2 || linedif == -2) && coldif == 0 &&\
-												(st[0] == 1 || st[0] == 6)) {
-			free (st);
-			free (en);
+		} else if ((linedif == 2 || linedif == -2) && coldif == 0 &&
+			((st[0] == 1 && board->cell[st[0]][st[1]].piececolor == Black) ||
+			(st[0] == 6 && board->cell[st[0]][st[1]].piececolor == White))) {
 			return 0;
+		} else {
+			return -1;
+		}
+		break;
+	case Rook:
+		if ((linedif != 0 && (linedif < 8 || linedif > -8)) && coldif == 0) {
+			if (linedif > 0) {
+				for (int line = st[0] - 1; line > en[0]; line--) {
+					if (board->cell[line][st[1]].fill) {
+						return -1;
+					}
+				}
+			} else if (linedif < 0) {
+				for (int line = st[0] + 1; line < en[0]; line++) {
+					if (board->cell[line][st[1]].fill) {
+						return -1;
+					}
+				}
+			}
+			return 0;
+		} else if ((coldif != 0 && (coldif < 8 || coldif > -8))
+		 													&& linedif == 0) {
+			if (coldif > 0) {
+				for (int col = st[1] - 1; col > en[1]; col--) {
+					if (board->cell[st[0]][col].fill) {
+						return -1;
+					}
+				}
+			} else if (coldif < 0) {
+				for (int col = st[1] + 1; col < en[1]; col++) {
+					if (board->cell[st[0]][col].fill) {
+						return -1;
+					}
+				}
+			}
+			return 0;
+		} else {
+			return -1;
+		}
+		break;
+	case Horse:
+		if (linedif == 2 || linedif == -2) {
+			if (coldif == 1 || coldif == -1) {
+				return 0;
+			}
+		} else if (coldif == 2 || coldif == -2) {
+			if (linedif == 1 || linedif == -1) {
+				return 0;
+			}
+		} else {
+			return -1;
 		}
 		break;
 
 	default:
-		free (st);
-		free (en);
 		return -1;
 		break;
 	}
 
-	free (st);
-	free (en);
 	return -1;
 }
 
 int move_make (char *move, Board *board) {
 
-	int *st = calloc (2, sizeof (int));
-	int *en = calloc (2, sizeof (int));
+	int st[2];
+	int en[2];
 
 	if (get_coord (move, st, en)) {
 		return -1;
@@ -194,8 +209,6 @@ int move_make (char *move, Board *board) {
 	board->cell[en[0]][en[1]].piecetype = board->cell[st[0]][st[1]].piecetype;
 	board->cell[en[0]][en[1]].piececolor = board->cell[st[0]][st[1]].piececolor;
 
-	free (st);
-	free (en);
 	return 0;
 }
 

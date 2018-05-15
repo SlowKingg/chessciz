7ad5fd586ef57f839acf7943662230d48e383e3a
Kirill <nagavi25599@gmail.com>
2018-05-16 01:25:59 +0700

    Add test for rook piece test

--- test/board_test.c
+++ test/board_test.c
@@ -50,3 +50,35 @@ CTEST(move_check, pawn_move_false)
 	ASSERT_EQUAL(expected, result);
 }
 
+CTEST(move_check, rook_move_true)
+{
+	// Given
+	char mv [7] = "h1h4\0";
+	Board * game = NULL;
+	game = init_board(game);
+	default_board(game);
+	game->cell[6][7].fill = false;
+
+	// When
+	const int result = move_check(mv, game);
+
+	// Then
+	const int expected = 0;
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(move_check, rook_move_false)
+{
+	// Given
+	char mv [7] = "h1h4\0";
+	Board * game = NULL;
+	game = init_board(game);
+	default_board(game);
+
+	// When
+	const int result = move_check(mv, game);
+
+	// Then
+	const int expected = -1;
+	ASSERT_EQUAL(expected, result);
+}
